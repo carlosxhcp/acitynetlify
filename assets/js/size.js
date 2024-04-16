b9c5e16e3493bca1240f2a3a17bfa79c975ca2a5
carlosxhcp <carlosxhcp@gmail.com>
2024-04-15 22:02:28 -0300

    A

--- assets/js/size.js
+++ assets/js/size.js
@@ -1,28 +1,35 @@
 function changeLinkHrefP() {
     const link = document.getElementById('link');
 
-    link.setAttribute('href', 'https://bobbyhadz.com');
+    link.setAttribute('href', 'https://acity.pay.yampi.com.br/r/QGUJDV4TAN');
 
     return false;
   }
 function changeLinkHrefM() {
     const link = document.getElementById('link');
 
-    link.setAttribute('href', 'https://bobbyhadz.com');
+    link.setAttribute('href', 'https://acity.pay.yampi.com.br/r/FJQ8IMTRR5');
 
     return false;
   }
   function changeLinkHrefG() {
     const link = document.getElementById('link');
 
-    link.setAttribute('href', 'https://www.youtube.com/watch?v=0a99Pe1qZ_0');
+    link.setAttribute('href', 'https://acity.pay.yampi.com.br/r/HYWXDWEVQA');
 
     return false;
   }
   function changeLinkHrefGG() {
     const link = document.getElementById('link');
 
-    link.setAttribute('href', 'https://moewalls.com/category/abstract/');
+    link.setAttribute('href', 'https://acity.pay.yampi.com.br/r/FQ9GVEPIEU');
+
+    return false;
+  }
+  function changeLinkHrefXG() {
+    const link = document.getElementById('link');
+
+    link.setAttribute('href', 'https://acity.pay.yampi.com.br/r/3QE8A9BFF8');
 
     return false;
   }
